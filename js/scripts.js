16617ae7f8bfacf6e266d2f56c9aa9ce1a879e9f
Alex Lauderdale <alauderdale@mac.com>
2012-10-13 18:36:58 -0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+
+
+jQuery(document).ready(function($) {
+
+
+      $('#gallery-1').royalSlider({
+          slidesSpacing: 0,
+          arrowsNav: false,
+          arrowsNav: true,
+          loop: true,
+          keyboardNavEnabled: true,
+          controlsInside: false,
+          imageScaleMode: 'fill',
+          arrowsNavAutoHide: false,
+          autoScaleSlider: true, 
+          arrowsNav: false,    
+          controlNavigation: 'thumbnails',
+          thumbsFitInViewport: true,
+          navigateByClick: true,
+          startSlideId: 0,
+          autoPlay: false,
+          transitionType:'move',
+          globalCaption: true, 
+          thumbs: {
+              		// thumbnails options go gere
+              		spacing: 0,
+              		arrows: false
+              	}
+        });
+        
+        
+        
+         
+      
+      
+      
+});
+
+
+      
+
+
+
+
+
+      
+
+
